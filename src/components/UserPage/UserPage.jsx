be46d8ed6f8fa2507b46fd2c7fefda739ca6f934
Josh Kralewski <joshkmanj@gmail.com>
2022-03-22 15:39:33 -0500

    Added inactive MUI component for later usage

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -33,122 +33,151 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 
 function UserPage() {
 
-  const user = useSelector((store)=> store.user)
-  const schedule = useSelector((store)=> store.schedule)
+  const user = useSelector((store) => store.user)
+  const schedule = useSelector((store) => store.schedule)
   const dispatch = useDispatch();
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     console.log('In useEffect');
-    dispatch({type:'FETCH_SHIFTS', payload: user.id})
-  },[])
+    dispatch({ type: 'FETCH_SHIFTS', payload: user.id })
+  }, [])
 
 
-//--------- MUI List item stuff --------
-const calendar = [
-  {
-    id: 1,
-    week_day_name: 'Monday',
-    shift_time: '7:00am - 3:30pm',
-    shift_date: '3/1',
-  },
-  {
-    id: 2,
-    week_day_name: 'Tuesday',
-    shift_time: '7:00am - 3:30pm',
-    shift_date: '3/2',
-  },
-  {
-    id: 3,
-    week_day_name: 'Wednesday',
-    shift_time: 'off :)',
-    shift_date: '3/3',
-  },
-  {
-    id: 4,
-    week_day_name: 'Thursday',
-    shift_time: 'off :)',
-    shift_date: '3/4',
-  },
-  {
-    id: 5,
-    week_day_name: "Friday",
-    shift_time: '7:00am - 3:30pm',
-    shift_date: '3/5',
-  },
-  {
-    id: 6,
-    week_day_name: 'Saturday',
-    shift_time: '7:00am - 3:30pm',
-    shift_date: '3/16',
-  },
-  {
-    id: 7,
-    week_day_name: 'Sunday',
-    shift_time: '7:00am - 3:30pm',
-    shift_date: '12/17',
-  },
-];
+  //--------- MUI List item stuff --------
+  const calendar = [
+    {
+      id: 1,
+      week_day_name: 'Monday',
+      shift_time: '7:00am - 3:30pm',
+      shift_date: '3/1',
+    },
+    {
+      id: 2,
+      week_day_name: 'Tuesday',
+      shift_time: '7:00am - 3:30pm',
+      shift_date: '3/2',
+    },
+    {
+      id: 3,
+      week_day_name: 'Wednesday',
+      shift_time: 'off :)',
+      shift_date: '3/3',
+    },
+    {
+      id: 4,
+      week_day_name: 'Thursday',
+      shift_time: 'off :)',
+      shift_date: '3/4',
+    },
+    {
+      id: 5,
+      week_day_name: "Friday",
+      shift_time: '7:00am - 3:30pm',
+      shift_date: '3/5',
+    },
+    {
+      id: 6,
+      week_day_name: 'Saturday',
+      shift_time: '7:00am - 3:30pm',
+      shift_date: '3/16',
+    },
+    {
+      id: 7,
+      week_day_name: 'Sunday',
+      shift_time: '7:00am - 3:30pm',
+      shift_date: '12/17',
+    },
+  ];
 
 
-//--------- MUI List item stuff --------
-/*
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-let yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-// document.write(today);
-*/
-
-// let todaysDate = (today);
-// console.log('current day is:', todaysDate);
+  //--------- MUI List item stuff --------
+  /*
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
   
+  today = mm + '/' + dd + '/' + yyyy;
+  // document.write(today);
+  */
+
+  // let todaysDate = (today);
+  // console.log('current day is:', todaysDate);
+
   return (
 
-<>
-{/* Top app bar */}
-<NavBar />
+    <>
+      {/* Top app bar */}
+      <NavBar />
+      {/* <List
+        sx={{
+          width: '100%',
+          maxWidth: 360,
+          bgcolor: 'background.paper',
+          position: 'relative',
+          overflow: 'auto',
+          maxHeight: 300,
+          '& ul': { padding: 0 },
+        }}
+        subheader={<li />}
+      >
+        {[0, 1, 2, 3, 4].map((sectionId) => (
+          <li key={`section-${sectionId}`}>
+            <ul>
+              <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
+              {schedule.map(({ id, current_week_number, week_number, week_day_name, shift_time, shift_date }) => (
+                <ListItem button>
+                  <ListItemAvatar>
+                    {shift_date.length < 4 ? (<Avatar sx={{ fontSize: 'medium' }}>{shift_date}</Avatar>) : (<Avatar sx={{ fontSize: 'small' }}>{shift_date}</Avatar>)}
+                  </ListItemAvatar>
+                  <ListItemText primary={week_day_name} secondary={shift_time} />
+                </ListItem>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </List> */}
 
-<React.Fragment>
-      <CssBaseline />
-      <Paper square sx={{ pb: '50px' }}>
-        <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
-          Schedule
-        </Typography>
-        <List sx={{ mb: 2 }}>
-          {schedule.map(({ id, current_week_number, week_number, week_day_name, shift_time, shift_date }) => (
-            <React.Fragment key={id}>
 
-{/*------- Here's logic that gives a subheader to list items dynamically ------*/}
-              { week_day_name === 'Monday' && (
-                <ListSubheader sx={{ bgcolor: 'background.paper' }}>
-                  Current Week
-                </ListSubheader>
-              )}
-              {/* {id === 3 && (
+      <React.Fragment>
+        <CssBaseline />
+        <Paper square sx={{ pb: '50px' }}>
+          <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
+            Schedule
+          </Typography>
+          <List sx={{ mb: 2 }}>
+            {schedule.map(({ id, current_week_number, week_number, week_day_name, shift_time, shift_date }) => (
+              <React.Fragment key={id}>
+
+                {/*------- Here's logic that gives a subheader to list items dynamically ------*/}
+                {week_day_name === 'Monday' && (
+                  <ListSubheader sx={{ bgcolor: 'background.paper' }}>
+                    Current Week
+                  </ListSubheader>
+                )}
+                {/* {id === 3 && (
                 <ListSubheader sx={{ bgcolor: 'background.paper' }}>
                   Next Week
                 </ListSubheader>
               )} */}
 
-              <ListItem button>
-                <ListItemAvatar>
-                {shift_date.length < 4 ? (<Avatar sx={{ fontSize: 'medium' }}>{shift_date}</Avatar>) : (<Avatar sx={{ fontSize: 'small' }}>{shift_date}</Avatar>)}
-                </ListItemAvatar>
-                <ListItemText primary={week_day_name} secondary={shift_time} />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
-      </Paper>
+                <ListItem button>
+                  <ListItemAvatar>
+                    {shift_date.length < 4 ? (<Avatar sx={{ fontSize: 'medium' }}>{shift_date}</Avatar>) : (<Avatar sx={{ fontSize: 'small' }}>{shift_date}</Avatar>)}
+                  </ListItemAvatar>
+                  <ListItemText primary={week_day_name} secondary={shift_time} />
+                </ListItem>
+              </React.Fragment>
+            ))}
+          </List>
+        </Paper>
 
 
 
 
-    </React.Fragment>
+      </React.Fragment>
 
-</>
+    </>
 
 
     // <div className="container">
