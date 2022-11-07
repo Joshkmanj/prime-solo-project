0a1d26e19c8bb617f3ac0c1cc8d851064ddaa634
Josh Kralewski <joshkmanj@gmail.com>
2022-11-07 15:25:49 -0600

    Recentered login page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -11,9 +11,9 @@ function LoginPage() {
 
   return (
     <div className="landing-grid">
+      <center>
       <LoginForm />
 
-      <center>
         <button
           type="button"
           className="btn btn_asLink"
